a9f1d078214c5ca3b2a746a3f3a4d9089e435260
tclass <3B95D43F-8A810419-9F1490A8-7B8A9214-146F0119>
2023-02-17 14:06:45 +0100

    fixing import

--- src/main/java/com/sesame/challenge/configuration/AppConfiguration.java
+++ src/main/java/com/sesame/challenge/configuration/AppConfiguration.java
@@ -2,6 +2,7 @@ package com.sesame.challenge.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Clock;
 
